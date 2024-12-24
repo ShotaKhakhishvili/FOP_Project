81b95e3a9364e8c04910bb5fdff33056ec3c8a4b
ShotaKhakhishvili <shotakhakhishvili@gmail.com>
2024-12-24 14:34:51 +0400

    Simple class to store variables

---
+++ Interpreter/src/Variables.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Variables<T> {
+    private Map<String,T> map = new HashMap<>();
+
+    public void declareVariable(String varName, T varValue){
+        if(map.containsKey(varName))
+            throw new RuntimeException("Variable '" + varName + "' is already declared");
+
+        map.put(varName, varValue);
+    }
+
+    public void setValue(String varName, T varValue){
+        if(map.containsKey(varName))
+            map.put(varName,varValue);
+        throw new RuntimeException("Variable '" + varName + "' is not declared");
+    }
+
+    public T getValue(String varName){
+        if(map.containsKey(varName))
+            return map.get(varName);
+
+        throw new RuntimeException("Variable '" + varName + "' is not declared");
+    }
+}

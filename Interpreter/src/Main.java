cf64df13ea990f9a892cfc747766baf35114b175
ShotaKhakhishvili <shotakhakhishvili@gmail.com>
2024-12-25 14:23:44 +0400

    Fixed commit

--- Interpreter/src/Main.java
+++ Interpreter/src/Main.java
@@ -1,10 +1,10 @@
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.Arrays;
 import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws FileNotFoundException  {
         Runner runner = new Runner("program.txt");
         runner.run();
     }

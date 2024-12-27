bff1e9eb31152fd7d965a13385768443a19c0410
mishakakhniashvili <misha.kakhniashvili3@gmail.com>
2024-12-27 18:02:55 +0400

    Minor changes
    Bug fixes, comments, adjustments

--- Interpreter/src/AssignmentHandler.java
+++ Interpreter/src/AssignmentHandler.java
@@ -1,19 +1,39 @@
 public class AssignmentHandler {
+
+    /**
+     * Executes an assignment operation by evaluating an expression and assigning the result to a variable.
+     *
+     * @param args The arguments passed for the assignment. The first argument is the variable name, the second argument is the operator, and the rest are the expression components.
+     * @throws CompilationError if there is an error during the execution of the expression.
+     */
     static void executeAssignment(String[] args) throws CompilationError {
+        // Prepare an array to hold the parts of the expression, excluding the first two arguments
         String[] expressionArgs = new String[args.length - 2];
 
-        for(int i = 2; i < args.length; i++){
-            expressionArgs[i-2] = args[i];
+        // Populate the expressionArgs array starting from the third element in args
+        for (int i = 2; i < args.length; i++) {
+            expressionArgs[i - 2] = args[i];
         }
 
+        // Evaluate the expression and assign the result to the variable identified by args[0]
         Runner.intStack.peek().setValue(args[0], ExpressionInteger.executeExpressionInteger(expressionArgs));
     }
 
-    public static boolean isNumber(String str){
-        for(char ch : str.toCharArray()){
-            if(!(ch <= '9' && ch >= '0'))
+    /**
+     * Checks whether a given string is a numeric value.
+     *
+     * @param str The string to check.
+     * @return True if the string represents a number, false otherwise.
+     */
+    public static boolean isNumber(String str) {
+        // Iterate through each character in the string
+        for (char ch : str.toCharArray()) {
+            // If the character is not a digit, return false
+            if (!(ch >= '0' && ch <= '9')) {
                 return false;
+            }
         }
+        // If all characters are digits, return true
         return true;
     }
 }
--- Interpreter/src/DeclarationHandler.java
+++ Interpreter/src/DeclarationHandler.java
@@ -1,9 +1,13 @@
 public class DeclarationHandler {
 
-    public static void executeDeclaration(String[] args){
-
+    /**
+     * Executes a declaration operation by declaring a new variable in the current scope.
+     *
+     * @param args The arguments for the declaration. The second argument (args[1]) is the variable name.
+     */
+    public static void executeDeclaration(String[] args) {
+        // Declares a variable in the current scope with a default value of null
         Runner.intStack.peek().declareVariable(args[1], null);
-
     }
 
 }
